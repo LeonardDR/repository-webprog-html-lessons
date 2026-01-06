a9c575c6107d47771d7bbbd5dd0fbad5ab44cd8f
Leonard Delos Reyes <ldelosreyes@student.apc.edu.ph>
2026-01-07 03:08:49 +0800

    cleaner ver of Midterm Project

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -6,27 +6,42 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const originalName = "Leonard";
     const originalStatus = "IT Student";
+    let resetTimer;
 
     boxes.forEach((box, index) => {
         box.style.opacity = "0";
-        box.style.transform = "translateY(30px)";
-        box.style.transition = "all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+        box.style.transform = "scale(0.9)";
+        box.style.transition = "all 0.5s ease-out";
         
         setTimeout(() => {
             box.style.opacity = "1";
-            box.style.transform = "translateY(0)";
-        }, 150 * index); 
+            box.style.transform = "scale(1)";
+        }, 100 * index); 
     });
 
     clickableBoxes.forEach(box => {
         box.addEventListener('click', function() {
-            const newInfo = this.getAttribute('data-info');
-            const categoryTitle = this.querySelector('h3').innerText;
-            updateCenterContent(categoryTitle, newInfo);
-            highlightBox(this);
+            const info = this.getAttribute('data-info');
+            const title = this.querySelector('h3').innerText;
+            
+            updateCenter(title, info);
+            
+            this.style.backgroundColor = "white";
+            setTimeout(() => { this.style.backgroundColor = ""; }, 300);
         });
     });
 
-   
+    function updateCenter(title, message) {
+        clearTimeout(resetTimer);
+        
+        profileName.innerText = title;
+        profileStatus.innerText = message;
+        profileStatus.style.fontSize = "0.85rem";
 
+        resetTimer = setTimeout(() => {
+            profileName.innerText = originalName;
+            profileStatus.innerText = originalStatus;
+            profileStatus.style.fontSize = "1rem";
+        }, 5000);
+    }
 });
